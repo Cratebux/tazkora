5233b81ba41f344690c91f205a2b6aafcd162453
Tony Oloriogun <webdevloper186@gmail.com>
2025-02-12 13:34:17 +0100

    completed the auth and home page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,7 +1,56 @@
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+"use client"
+import Image from "next/image";
+import signUpSvg from "../../../../public/icons/signup.svg";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
-export default Login
+const Login = () => {
+  const [email, setEmail] = useState("")
+  const [verify, setVerify] =useState(false)
+  const router = useRouter()
+
+  const handleVerification = async() => {
+      const data = await fetch('https://localhost:5000/send-verification', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'}, 
+        body: JSON.stringify({email})
+      })
+      
+      const response = await data.json();
+      if(response.ok) {
+        router.push('/verify')
+      }else{
+        alert("Login Failed")
+      }
+  }
+
+  
+  return (
+    <div className="bg-backgroundDark w-full h-screen flex justify-center items-center">
+      <div className="bg-backgroundLight mx-5 lg:mx-0 px-5 text-white w-[361px] h-[330px] rounded-[14px] flex flex-col justify-center">
+        <Image
+          className="pb-3"
+          src={signUpSvg}
+          alt="icon"
+          width="64"
+          height="64"
+        />
+        <h1 className="font-bold text-2xl">Welcome to Tazkora</h1>
+        <p className="">Please sign in or sign below</p>
+        <div className="pt-5">
+          <label className="font-semibold">Email:</label>
+          <input
+            className="bg-backgroundDark w-full mt-3 outline-none py-3 pl-5 rounded-[8px]"
+            placeholder="riderezzy@gmail.com"
+            onChange={(e) => {setEmail(e.target.value)}}
+          />
+          <button onClick={handleVerification} className="border-backgroundDark border w-full py-3 mt-3 rounded-[8px]">
+            Continue with email
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,5 +1,31 @@
+import Header from "@/app/component/Header";
+import Partners from "@/app/component/Partners";
+import Tasks from "../component/Tasks";
+import gamebanner from '../../../public/images/gamebanner.png'
+import gamebanner2 from '../../../public/images/gamebanner2.png'
+import Image from "next/image";
+import QuickTasks from "../component/QuickTasks";
+import Surveys from "../component/Surveys";
 const Home = () => {
-  return <div>Home</div>;
+  return(
+    <div className="bg-backgroundDark">
+      <Header/>
+      <Partners/>
+      <Tasks/>
+      <Image
+        src={gamebanner}
+        alt="gamebanner"
+        className="w-full lg:px-10 px-5 pt-10"
+      />
+      <QuickTasks/>
+      <Surveys/>
+      <Image
+        src={gamebanner2}
+        alt="gamebanner"
+        className="w-full lg:px-10 px-5 py-10"
+      />
+    </div>
+  )
 };
 
-export default Home;
+export default Home
