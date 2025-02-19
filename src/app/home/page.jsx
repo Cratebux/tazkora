0dc3175e6590a3f2befb00767d4c16603fb1307d
CosMo578 <raphaelakpor00@gmail.com>
2025-02-19 12:22:58 +0100

    Added Footer to the Home page

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,31 +1,35 @@
 import HomeHeader from "../../components/HomeHeader";
 import Partners from "@/components/Partners";
 import Tasks from "../../components/Tasks";
-import gamebanner from '../../../public/images/gamebanner.png'
-import gamebanner2 from '../../../public/images/gamebanner2.png'
+import gamebanner from "../../../public/images/gamebanner.png";
+import gamebanner2 from "../../../public/images/gamebanner2.png";
 import Image from "next/image";
 import QuickTasks from "../../components/QuickTasks";
 import Surveys from "../../components/Surveys";
+import ProfileFooter from "@/components/profile/ProfileFooter";
 const Home = () => {
-  return(
+  return (
     <div className="bg-profilebg">
-      <HomeHeader/>
-      <Partners/>
-      <Tasks/>
+      <HomeHeader />
+      <Partners />
+      <Tasks />
       <Image
         src={gamebanner}
         alt="gamebanner"
-        className="w-full lg:px-10 px-5 pt-10"
+        className="w-full px-5 pt-10 lg:px-10"
       />
-      <QuickTasks/>
-      <Surveys/>
+      <QuickTasks />
+      <Surveys />
       <Image
         src={gamebanner2}
         alt="gamebanner"
-        className="w-full lg:px-10 px-5 py-10"
+        className="w-full px-5 py-10 lg:px-10"
       />
+      <div className="px-6 lg:px-16">
+        <ProfileFooter />
+      </div>
     </div>
-  )
+  );
 };
 
-export default Home
+export default Home;
