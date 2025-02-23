1d2bc69594565ad71480e5fecfcc0c6a2708bdd9
Tony Oloriogun <webdevloper186@gmail.com>
2025-02-23 15:25:17 +0100

    Modified some pages

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import React from "react";
-import warning from '../../../public/svg/!.svg'
 import MyReward from "@/components/profile/MyReward";
 import RecentPayout from "@/components/profile/RecentPayout";
 import RecommendedTask from "@/components/profile/RecommendedTasks";
