7230e1090f4a7677729ae891b51a930cb264d4bb
CosMo578 <raphaelakpor00@gmail.com>
2025-04-16 17:03:42 +0100

    Fixed formatting for middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,17 +1,15 @@
 import { NextResponse } from "next/server";
 import { token } from "./app/home/page";
 
-console.log(token)
+console.log(token);
 
 export const middleware = (request) => {
-    if(!token){
-        return NextResponse.redirect(
-            new URL('/login', request.url)
-        )
-    }
-    return NextResponse.next()
-}
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+  return NextResponse.next();
+};
 
 export const config = {
-    matcher: [ '/profile', '/home' ]
-}
+  matcher: ["/profile", "/home"],
+};
